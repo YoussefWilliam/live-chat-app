1b88ce6f7806de6120713a38e3714ae97b7a79a4
YoussefWilliam <youssef.william997@gmail.com>
2022-12-02 14:08:55 +0200

    adding websocket

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,27 @@
-console.log("here!");
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
+import cors from "cors";
+
+const port = 8080;
+const app = express();
+app.use(cors());
+const httpServer = http.createServer(app);
+const io = new Server(httpServer, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("🥳User is now connected 🥳");
+
+  socket.on("disconnect", () => {
+    console.log("🥺 User is disconnected 🥺");
+  });
+});
+
+httpServer.listen(port, () => {
+  console.log("🚀 Listining on port", port, "🚀");
+});
